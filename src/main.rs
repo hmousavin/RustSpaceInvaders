1d319b75f88bae307d135ec899ee3de4819ca651
hmousavin <hamid.mousavinasab@gmail.com>
2025-04-16 08:19:34 +0330

    added components to setup + apply_user_input

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,38 @@ use bevy::{
     window::ExitCondition,
 };
 
+const CANNON_STEP: f32 = 100.;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Cannon {
+    lives: u8,
+    can_shoot: bool,
+}
+
+#[derive(Component)]
+struct CannonBall;
+
+enum AlienType {
+    Squid,
+    Crab,
+    Octopus,
+    UFO,
+}
+
+#[derive(Component)]
+struct Alien {
+    kind_of: AlienType,
+    pos: Position,
+    is_alive: bool,
+    // score_value: u32, // should be randomized: 50, 100, 150, 300
+}
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<&Window>) {
     let window = windows.single(); // assumes one window
     let screen_width = window.resolution.width();
@@ -23,28 +55,48 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<
     commands.spawn((
         Sprite::from_image(asset_server.load("player_38.png")),
         Transform::from_xyz(0., bottom, 0.),
+        Cannon {
+            lives: 3,
+            can_shoot: true,
+        },
     ));
 
     for i in -3..4 {
         commands.spawn((
             Sprite::from_image(asset_server.load("green_3.png")),
             Transform::from_xyz(sprite_pad.mul(i as f32), margin_top.mul(8.), 0.),
+            Alien {
+                kind_of: AlienType::Squid,
+                pos: Position { x: sprite_pad.mul(i as f32), y: margin_top.mul(8.) },
+                is_alive: true
+            },
         ));
 
         commands.spawn((
             Sprite::from_image(asset_server.load("yellow_5.png")),
             Transform::from_xyz(sprite_pad.mul(i as f32), margin_top.mul(4.), 0.),
+            Alien {
+                kind_of: AlienType::Crab,
+                pos: Position { x: sprite_pad.mul(i as f32), y: margin_top.mul(4.) },
+                is_alive: true
+            },
         ));
 
         commands.spawn((
             Sprite::from_image(asset_server.load("red_5.png")),
             Transform::from_xyz(sprite_pad.mul(i as f32), margin_top, 0.),
+            Alien {
+                kind_of: AlienType::Octopus,
+                pos: Position { x: sprite_pad.mul(i as f32), y: margin_top },
+                is_alive: true
+            },
         ));
     }
 
     commands.spawn((
         Sprite::from_image(asset_server.load("white_arrow.png")),
         Transform::from_xyz(0., -margin_bottom.mul(2.), 0.),
+        CannonBall,
     ));
 
     commands.spawn((
@@ -55,6 +107,28 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<
             0.,
         ),
     ));
+}
+
+fn apply_user_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut cannon_transform: Single<&mut Transform, With<Cannon>>,
+    time: Res<Time>,
+) {
+    let mut direction = 0.0;
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        direction -= CANNON_STEP;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        direction += CANNON_STEP;
+    }
+
+    if keyboard_input.just_released(KeyCode::Space) {
+        info!("kew ! kew 1");
+        todo!("call/implement the shoot logic");
+    }
+
+    cannon_transform.translation.x += direction * time.delta_secs();
 }
 
 fn main() {
@@ -75,5 +149,6 @@ fn main() {
                 }),
         )
         .add_systems(Startup, setup)
+        .add_systems(Update, apply_user_input)
         .run();
 }
