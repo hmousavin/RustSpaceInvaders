7864d5fc12585a6e6976554f33a7a18caacafbba
hmousavin <hamid.mousavinasab@gmail.com>
2025-04-19 16:49:33 +0330

    put a contraint on cannon range of move + chores

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::ops::Mul;
+use std::ops::{Add, Mul};
 
 use bevy::{
-    prelude::*, render::{
+    prelude::*,
+    render::{
         settings::{Backends, RenderCreation, WgpuSettings},
         RenderPlugin,
-    }, window::ExitCondition
+    },
+    window::ExitCondition,
 };
 
 const CANNON_STEP: f32 = 10.;
@@ -12,12 +14,11 @@ const CANNON_STEP: f32 = 10.;
 #[derive(Component)]
 struct BoundingBox {
     width: f32,
-    height: f32
+    height: f32,
 }
 
 #[derive(Component)]
-struct Cannon {
-}
+struct Cannon {}
 
 #[derive(Copy, Clone)]
 enum BallType {
@@ -44,11 +45,23 @@ struct Alien {
 }
 
 fn get_alien_bounding_box(alien_type: AlienType) -> BoundingBox {
-    let bb =  match alien_type {
-        AlienType::Squid => BoundingBox { width: 40., height: 32. },
-        AlienType::Crab => BoundingBox { width: 40., height: 32. },
-        AlienType::Octopus => BoundingBox { width: 40., height: 32. },
-        AlienType::UFO => BoundingBox { width: 40., height: 20. },
+    let bb = match alien_type {
+        AlienType::Squid => BoundingBox {
+            width: 40.,
+            height: 32.,
+        },
+        AlienType::Crab => BoundingBox {
+            width: 40.,
+            height: 32.,
+        },
+        AlienType::Octopus => BoundingBox {
+            width: 40.,
+            height: 32.,
+        },
+        AlienType::UFO => BoundingBox {
+            width: 40.,
+            height: 20.,
+        },
     };
 
     bb
@@ -56,7 +69,10 @@ fn get_alien_bounding_box(alien_type: AlienType) -> BoundingBox {
 
 fn get_ball_bounding_box(ball_type: BallType) -> BoundingBox {
     let bb = match ball_type {
-        BallType::CannonBall => BoundingBox { width: 4., height: 10. },
+        BallType::CannonBall => BoundingBox {
+            width: 4.,
+            height: 10.,
+        },
     };
 
     bb
@@ -64,12 +80,13 @@ fn get_ball_bounding_box(ball_type: BallType) -> BoundingBox {
 
 #[derive(PartialEq, Eq)]
 enum Direction {
-    Left, Right
+    Left,
+    Right,
 }
 
 #[derive(Resource)]
 struct AlienMoveDirection {
-    dir: Direction
+    dir: Direction,
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>, window: Single<&Window>) {
@@ -85,7 +102,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, window: Single<
     commands.spawn((
         Sprite::from_image(asset_server.load("cannon.png")),
         Transform::from_xyz(0., bottom, 0.),
-        Cannon{}
+        Cannon {},
     ));
 
     for i in -3..4 {
@@ -123,7 +140,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, window: Single<
         ),
         Alien {
             kind_of: AlienType::UFO,
-        }
+        },
     ));
 }
 
@@ -131,20 +148,29 @@ fn handle_inputs(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut cannon_transform: Single<&mut Transform, With<Cannon>>,
     mut commands: Commands,
-    asset_server: Res<AssetServer>, 
+    asset_server: Res<AssetServer>,
+    window: Single<&Window>,
 ) {
     if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        cannon_transform.translation.x -= CANNON_STEP;
+        if -window.width()/2. < cannon_transform.translation.x - CANNON_STEP {
+            cannon_transform.translation.x -= CANNON_STEP;
+        }
     }
 
     if keyboard_input.pressed(KeyCode::ArrowRight) {
-        cannon_transform.translation.x += CANNON_STEP;
+        if cannon_transform.translation.x + CANNON_STEP < window.width()/2. {
+            cannon_transform.translation.x += CANNON_STEP;
+        }
     }
 
     if keyboard_input.just_pressed(KeyCode::Space) {
         commands.spawn((
             Sprite::from_image(asset_server.load("cannon_ball.png")),
-            Transform::from_xyz(cannon_transform.translation.x, cannon_transform.translation.y+20., 0.),
+            Transform::from_xyz(
+                cannon_transform.translation.x,
+                cannon_transform.translation.y + 20.,
+                0.,
+            ),
             Ball {
                 kind_of: BallType::CannonBall,
             },
@@ -154,14 +180,15 @@ fn handle_inputs(
 
 fn refresh_aliens(
     mut alien_transforms: Query<&mut Transform, With<Alien>>,
-    mut alien_direction: ResMut<AlienMoveDirection>, 
+    mut alien_direction: ResMut<AlienMoveDirection>,
     window: Single<&Window>,
 ) {
     let screen_left = -window.width() / 2.;
     let screen_right = window.width() / 2.;
-    
+
     for mut transform in alien_transforms.iter_mut() {
-        if alien_direction.dir == Direction::Right && transform.translation.x + 20. >= screen_right {
+        if alien_direction.dir == Direction::Right && transform.translation.x + 20. >= screen_right
+        {
             alien_direction.dir = Direction::Left;
             break;
         }
@@ -169,12 +196,10 @@ fn refresh_aliens(
             alien_direction.dir = Direction::Right;
             break;
         }
-    
-        if alien_direction.dir == Direction::Left {
-            transform.translation.x -= 2.;
-        }
-        else {
-            transform.translation.x += 2.;
+
+        match alien_direction.dir {
+            Direction::Left => transform.translation.x -= 2.,
+            Direction::Right => transform.translation.x += 2.,
         }
     }
 }
@@ -208,7 +233,6 @@ fn check_collisions(
 ) {
     for (ball_entity, ball_transform, ball) in balls.iter() {
         for (alien_entity, alien_transform, alien) in aliens.iter() {
-
             let ball_pos = ball_transform.translation;
             let alien_pos = alien_transform.translation;
 
@@ -222,10 +246,10 @@ fn check_collisions(
             let alien_min_y = alien_pos.y - get_alien_bounding_box(alien.kind_of).height / 2.0;
             let alien_max_y = alien_pos.y + get_alien_bounding_box(alien.kind_of).height / 2.0;
 
-            if ball_min_x < alien_max_x &&
-            ball_max_x > alien_min_x &&
-            ball_min_y < alien_max_y &&
-            ball_max_y > alien_min_y
+            if ball_min_x < alien_max_x
+                && ball_max_x > alien_min_x
+                && ball_min_y < alien_max_y
+                && ball_max_y > alien_min_y
             {
                 commands.entity(alien_entity).despawn();
                 commands.entity(ball_entity).despawn();
@@ -252,8 +276,18 @@ fn main() {
                     ..Default::default()
                 }),
         )
-        .insert_resource(AlienMoveDirection{dir: Direction::Right})
+        .insert_resource(AlienMoveDirection {
+            dir: Direction::Right,
+        })
         .add_systems(Startup, setup)
-        .add_systems(Update, (handle_inputs, refresh_aliens, refresh_balls, check_collisions))
+        .add_systems(
+            Update,
+            (
+                handle_inputs,
+                refresh_aliens,
+                refresh_balls,
+                check_collisions,
+            ),
+        )
         .run();
 }
